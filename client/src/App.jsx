68fed85a464abc6804389acca19e979d45c405da
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-10-13 22:18:48 +0530

    Vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import PlacePage from "./pages/PlacePage";
 // import BookingsPage from "./pages/BookingsPage";
 // import BookingPage from "./pages/BookingPage";
 
-axios.defaults.baseURL = "http://localhost:4000/api";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
