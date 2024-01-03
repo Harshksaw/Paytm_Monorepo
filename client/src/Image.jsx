c3e52e81ce3ffe39c43a67b129848d03c9cfc496
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-01-03 14:58:58 +0000

    Taxi-booking Init

--- client/src/Image.jsx
+++
@@ -1,8 +0,0 @@
-export default function Image({src,...rest}) {
-  src = src && src.includes('https://')
-    ? src
-    : 'http://localhost:4000/uploads/'+src;
-  return (
-    <img {...rest} src={src} alt={''} />
-  );
-}
