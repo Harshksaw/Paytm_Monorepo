6350b6363d32a20fbcd0586337f7ebd16e7344be
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-02-19 15:42:48 +0000

    Add mobile sidebar functionality and textarea component

--- app/(platform)/(clerk)/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(platform)/(clerk)/(dashboard)/_components/mobile-sidebar.tsx
@@ -6,16 +6,32 @@ import { usePathname } from "next/navigation";
 
 
 import { Button } from "@/components/ui/button";
+import { useMobileSidebar } from "@/hooks/use-mobile-sidebar";
+import { Sidebar } from "./sidebar";
+import { Sheet, SheetContent } from "@/components/ui/sheet";
 
 
 export const MobileSidebar = () => {
+
+
+  const onOpen = useMobileSidebar((state) => state.onOpen);
+  const onClose = useMobileSidebar((state) => state.onClose);
+  const isOpen = useMobileSidebar((state) => state.isOpen);
+
+
   const pathname = usePathname();
   const [isMounted, setIsMounted] = useState(false);
 
 
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
- 
- 
+  useEffect(() => {
+    onClose();
+  }, [pathname, onClose]);
+
+
   if (!isMounted) {
     return null;
   }
@@ -23,14 +39,32 @@ export const MobileSidebar = () => {
   return (
     <>
       <Button
-        // onClick={onOpen}
+        onClick={onOpen}
         className="block md:hidden mr-2"
         variant="ghost"
         size="sm"
       >
         <Menu className="h-4 w-4" />
+
+        <Sheet
+          open={isOpen}
+          onOpenChange={onClose}
+
+        >
+
+          <SheetContent
+            side="left"
+            className="p-2 py-10"
+          >
+            <Sidebar
+              storageKey="t-sidebar-mobile-state"
+            />
+          </SheetContent>
+        </Sheet>
+
+
       </Button>
-     
+
     </>
   )
 }
--- app/(platform)/(clerk)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(clerk)/(dashboard)/_components/sidebar.tsx
@@ -7,6 +7,8 @@ import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import {Button} from "@/components/ui/button";
 import {Separator} from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "@/components/ui/accordion";
+import { NavItem, Organization } from "./nav-item";
 
 interface SidebarProps{
     storageKey?: string;
@@ -40,6 +42,8 @@ export const Sidebar = ({
         
         }, []);
 
+        // {"123": true, "456": false} => ["123"]
+
     const onExpand = (id: string) => {
         setExapnded((curr)=> ({
             ...curr,
@@ -51,18 +55,63 @@ export const Sidebar = ({
     if(!isLoadedOrg || !isLoadedOrgsList || userMemberships.isLoading){
         return (
             <>
-                <Skeleton/>
+            <div className="flex items-center gap-x-4 justify-between mb-2">
+
+
+                <Skeleton className= "h-10 w-[50%] bg-sky-200"/>
+                <Skeleton className= "h-10 w-10 bg-sky-200"/>
+            </div>
             </>
         )
     }
 
 
     return (
-        <div>
-            sidebar
+        <>
+
+        <div className="font-medium text-xs flex items-center mv-10">
+            <span className="pl-4">
+
+                Workspaces
+
+            </span>
+            <Button
+            asChild
+            type="button"
+            size="icon"
+            variant="ghost"
+            className="ml-auto"
+            >
+                <Link href="/select-org">
+                    <Plus className="h-4 w-4">
+
+                    </Plus>
+                </Link>
+            </Button>
+
             
 
         </div>
+
+        <Accordion
+        type="multiple"
+        defaultValue={defaultAccordeionValue}
+        className="space-y-2"
+
+        >
+            {userMemberships.data.map(({organization})=>(
+                    <NavItem
+                    key={organization.id}
+                    isActive={activeOrganization?.id === organization.id}
+                    isExpanded={expanded[organization.id]}
+                    organization={organization as Organization}
+                    onExpand={onExpand}
+                    />
+
+            ))} 
+
+        </Accordion>
+        </>
     )
 
 
