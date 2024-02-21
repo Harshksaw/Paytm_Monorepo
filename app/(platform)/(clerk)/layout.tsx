4ddbeb9eeadcadc8a71b3a40e0cb5b2fe7aa8e8c
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-02-21 12:35:50 +0000

    ready

--- app/(platform)/(clerk)/layout.tsx
+++ app/(platform)/(clerk)/layout.tsx
@@ -2,7 +2,7 @@ const ClerkLayout = ({ children }: {
   children: React.ReactNode;
 }) => {
   return (
-    <div className="h-full flex items-center ">
+    <div className="h-full flex items-center justify-center bg-slate-100 mt-[30%]">
       {children}
     </div>
   );
