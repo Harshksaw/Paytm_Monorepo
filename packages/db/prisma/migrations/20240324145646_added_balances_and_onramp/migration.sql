06857cb3bff7c3d5066236b7ea287a7ee8f584e4
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-04-03 01:39:30 +0000

    Reinit Paytm Mono/TurboRepo

--- packages/db/prisma/migrations/20240324145646_added_balances_and_onramp/migration.sql
+++ packages/db/prisma/migrations/20240324145646_added_balances_and_onramp/migration.sql
@@ -1,29 +1,5 @@
 -- CreateEnum
-CREATE TYPE "AuthType" AS ENUM ('Google', 'Github');
-
--- CreateEnum
 CREATE TYPE "OnRampStatus" AS ENUM ('Success', 'Failure', 'Processing');
-
--- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "email" TEXT,
-    "name" TEXT,
-    "number" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Merchant" (
-    "id" SERIAL NOT NULL,
-    "email" TEXT NOT NULL,
-    "name" TEXT,
-    "auth_type" "AuthType" NOT NULL,
-
-    CONSTRAINT "Merchant_pkey" PRIMARY KEY ("id")
-);
 
 -- CreateTable
 CREATE TABLE "OnRampTransaction" (
@@ -47,15 +23,6 @@ CREATE TABLE "Balance" (
 
     CONSTRAINT "Balance_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_number_key" ON "User"("number");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Merchant_email_key" ON "Merchant"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "OnRampTransaction_token_key" ON "OnRampTransaction"("token");
