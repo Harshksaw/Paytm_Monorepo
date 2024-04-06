7e2074b0faedbe47ce1d55e4c577d79372f0b0cf
Harsh kumar <mister.harshkumar@gmail.com>
2024-04-06 18:41:36 +0530

    Update background color in RootLayout component

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <Providers>
         <body className={inter.className}>
-          <div className="min-w-screen min-h-screen bg-[#ebe6e6]">
+          <div className="min-w-screen min-h-screen bg-[#ebe6e6] opacity-1">
             <AppbarClient />
             {children}
           </div>
